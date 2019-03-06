f3d29265de86cf11506096d0abc3897dd18da603
Razvi99 <razvi.david@gmail.com>
2019-03-06 23:13:06 +0000

    Some model creation checks.

--- cw-model/src/main/java/uk/ac/bris/cs/scotlandyard/model/ScotlandYardModel.java
+++ cw-model/src/main/java/uk/ac/bris/cs/scotlandyard/model/ScotlandYardModel.java
@@ -1,19 +1,51 @@
 package uk.ac.bris.cs.scotlandyard.model;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 
 import uk.ac.bris.cs.gamekit.graph.Graph;
 
 // TODO implement all methods and pass all tests
 public class ScotlandYardModel implements ScotlandYardGame {
 
+	private List<Boolean> rounds;
+	private Graph<Integer, Transport> graph;
+
 	public ScotlandYardModel(List<Boolean> rounds, Graph<Integer, Transport> graph,
 			PlayerConfiguration mrX, PlayerConfiguration firstDetective,
 			PlayerConfiguration... restOfTheDetectives) {
-		// TODO
+
+		// Null and empty checks.
+		if(rounds != null) this.rounds = rounds;
+		else throw new NullPointerException("Rounds is null");
+
+		if(graph != null) this.graph = graph;
+		else throw new NullPointerException("Graph is null");
+
+		if(rounds.isEmpty()) throw new IllegalArgumentException("Rounds is empty");
+		if(graph.isEmpty()) throw new IllegalArgumentException("Graph is empty");
+
+		// mrX colour check.
+		if(mrX.colour != Colour.BLACK) throw new IllegalArgumentException("Mr. X should be black");
+
+		// Adding all players to a list.
+		ArrayList<PlayerConfiguration> players = new ArrayList<>(Arrays.asList(restOfTheDetectives)); // players is constructed with all elements of restOfDetectives
+		players.add(firstDetective);
+		players.add(mrX);
+
+		// Duplicate location or colour check.
+		HashSet<Integer> locations = new HashSet<>();
+		for(PlayerConfiguration p : players)
+			if(locations.contains(p.location))
+				throw new IllegalArgumentException("Duplicate location");
+			else locations.add(p.location);
+
+		HashSet<Colour> colours = new HashSet<>();
+		for(PlayerConfiguration p : players)
+			if(colours.contains(p.colour))
+				throw new IllegalArgumentException("Duplicate colour");
+			else colours.add(p.colour);
+
+		// Ticket check.
 	}
 
 	@Override
